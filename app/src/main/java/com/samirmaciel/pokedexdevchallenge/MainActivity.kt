1ae0d058c0091646089e798c6edb5b769340c7c7
samirmaciel <samirmaciel.dev@gmail.com>
2022-01-24 20:30:24 -0300

    first commit

---
+++ app/src/main/java/com/samirmaciel/pokedexdevchallenge/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.samirmaciel.pokedexdevchallenge
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
